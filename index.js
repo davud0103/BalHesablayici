158a4ea9391ede0fc0b8e937fe5adb9a067413b8
science2010 <157699973+davud0103@users.noreply.github.com>
2024-02-26 21:30:26 +0400

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,35 @@ $(".tesdiq-button").on("click", function() {
     var yazilmayanInput = $("#yazilmayan-input").val();
     var balInput = $("#bal-input").val();
 
-    if (umumiInput.length > 0 && duzgunInput.length > 0 && yazilmayanInput.length > 0 && balInput.length > 0 && umumiInput.length >= (duzgunInput.length + yazilmayanInput.length)) {
+    var intUmumi = parseFloat(umumiInput);
+    var intDuzgun = parseFloat(duzgunInput);
+    var intYazilmayan = parseFloat(yazilmayanInput);
+    var intBal = parseFloat(balInput);
+
+    if (umumiInput.length > 0 && duzgunInput.length > 0 && yazilmayanInput.length > 0 && balInput.length > 0 && intUmumi >= (intDuzgun + intYazilmayan)) {
         umumiInput = $("#umumi-input").val();
         duzgunInput = $("#duzgun-input").val();
         yazilmayanInput = $("#yazilmayan-input").val();
         balInput = $("#bal-input").val();
 
-        var intUmumi = parseFloat(umumiInput);
-        var intDuzgun = parseFloat(duzgunInput);
-        var intYazilmayan = parseFloat(yazilmayanInput);
-        var intBal = parseFloat(balInput);
+        intUmumi = parseFloat(umumiInput);
+        intDuzgun = parseFloat(duzgunInput);
+        intYazilmayan = parseFloat(yazilmayanInput);
+        intBal = parseFloat(balInput);
 
         var yanlisSualMiqdari = intUmumi - (intDuzgun + intYazilmayan);
         var sual4 = Math.floor(yanlisSualMiqdari/4);
-        var duzgunSual = intUmumi - (yanlisSualMiqdari + sual4);
+        var duzgunSual = intUmumi - (yanlisSualMiqdari + sual4 + intYazilmayan);
         var bal = duzgunSual*intBal;
-        $(".span-1").text(bal + " bal");
+        var umumiBalMiqdari = intUmumi * intBal;
+        var faiz = Math.floor((bal/umumiBalMiqdari)*100);
+        $(".span-1").text(bal + " bal(" + faiz + "%)"); 
     }
+    else {
+        $(".span-1").text("Səhvlik etmisiniz!");
+    }
+});
+
+$(".clear-button").on("click", function(){
+    $(".input-pointer").val('');
 });
